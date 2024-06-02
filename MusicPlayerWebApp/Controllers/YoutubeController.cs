692ffc0d1f148668e90ead375fa9257a3d0230ad
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2024-06-02 18:12:24 -0400

    Added Youtube/Fetch endpoint

---
+++ MusicPlayerWebApp/Controllers/YoutubeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MusicPlayerWebApp.Controllers
+{
+    public class YoutubeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Fetch(string videoQuery)
+        {
+            if (string.IsNullOrWhiteSpace(videoQuery))
+            {
+                return View("Index");
+            }
+
+            // Here you will add logic to process the videoQuery, either as a URL or search terms.
+            // For now, just return the same view with a placeholder message.
+            ViewBag.Message = $"Processed query: {videoQuery}";
+            return View("Index");
+        }
+    }
+}
