23e9691ea65ec9baa31795ff8da0e677061d9e32
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2024-06-04 11:23:02 -0400

    Decoupled the services from the main controller. Added services for API calls. Include Singleton pattern and DependencyInjection for YouTube Service.

---
+++ MusicPlayerWebApp/Models/VideoDetails.cs
@@ -0,0 +1,16 @@
+﻿namespace MusicPlayerWebApp.Models
+{
+    public class VideoDetails
+    {
+        public string VideoId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ThumbnailUrl { get; set; }  // Consider uncommenting in your view if needed
+        public long Views { get; set; }
+        public DateTimeOffset PublishedAt { get; set; }
+        public string ChannelId { get; set; }
+        public string ChannelTitle { get; set; }
+        public string ChannelUrl { get; set; }
+        public string ChannelIcon { get; set; }
+    }
+}
---
+++ MusicPlayerWebApp/Models/VideoSearchResult.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicPlayerWebApp.Models
+{
+    public class VideoSearchResult
+    {
+        public string VideoId { get; set; }
+        public string Title { get; set; }
+        public string ThumbnailUrl { get; set; }
+        public int Count { get; set; }
+    }
+}
