2a6a7778b65a21e67976db0843dd7b1d21914d72
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2024-06-05 11:21:09 -0400

    Added backend authentication service with AWS Cognito (connected application with AWS).

---
+++ MusicPlayerWebApp/Models/UserSession.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicPlayerWebApp.Models
+{
+    public class UserSession
+    {
+        public string IdToken { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
