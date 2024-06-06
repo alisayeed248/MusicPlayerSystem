fbbe0dca5fdbb8364585dd12d9fc2fc7594ca6a5
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2024-06-05 21:24:29 -0400

    Basic files for adding media service: will be decoupled and be a microservice

---
+++ MediaService/Controllers/VideoDownloadController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MediaService.Services;
+using System.Threading.Tasks;
+
+namespace MediaService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoDownloadController : ControllerBase
+    {
+        private readonly VideoDownloadService _downloadService;
+
+        public VideoDownloadController(VideoDownloadService downloadService)
+        {
+            _downloadService = downloadService;
+        }
+
+        [HttpPost("download")]
+        public async Task<IActionResult> DownloadVideo([FromBody] string videoUrl)
+        {
+            try
+            {
+                var filePath = await _downloadService.DownloadVideoAsync(videoUrl);
+                return Ok(filePath);
+            }
+            catch(System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ MediaService/Services/VideoDownloadService.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace MediaService.Services
+{
+    public class VideoDownloadService
+    {
+        public async Task<string> DownloadVideoAsync(string videoUrl)
+        {
+            // we'll need to change the output path to be using S3
+            string outputPath = "path_to_save_downloaded_file";
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "yt-dlp",
+                    Arguments = $"{videoUrl} -o {outputPath}",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            await process.WaitForExitAsync();
+
+            if (process.ExitCode == 0)
+            {
+                return outputPath; // Return the path of the downloaded file
+            }
+            else
+            {
+                throw new Exception("Failed to download video.");
+            }
+        }
+    }
+}
