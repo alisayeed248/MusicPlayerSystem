9c598e6e1628078c6766535b67298fcd4c238d40
Sayeed Ali <88571614+alisayeed248@users.noreply.github.com>
2024-06-06 21:15:43 -0400

    Added base mp3 download compatibility to MediaService and added front-end Download Mp3 and Download Mp4 buttons to Youtube Index.cshtml

--- MediaService/Services/VideoDownloadService.cs
+++ MediaService/Services/VideoDownloadService.cs
@@ -33,5 +33,35 @@ namespace MediaService.Services
                 throw new Exception("Failed to download video.");
             }
         }
+
+        public async Task<string> ConvertToMp3Async(string videoUrl)
+        {
+            string filename = $"{Guid.NewGuid()}.mp3";
+            string outputPath = Path.Combine("D:\\Sayeed\\Downloads", filename);
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "yt-dlp",
+                    Arguments = $"{videoUrl} --extract-audio --audio-format mp3 -o {outputPath}",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string standardOutput = await process.StandardOutput.ReadToEndAsync();
+            string standardError = await process.StandardError.ReadToEndAsync();
+            await process.WaitForExitAsync();
+
+            if (process.ExitCode == 0)
+            {
+                return outputPath;
+            }
+            else
+            {
+                throw new Exception($"Failed to convert video to MP3. Error: {standardError}");
+            }
+        }
     }
 }
